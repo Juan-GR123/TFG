18c386c40c5900b70d905261d098abb50186628f
Juan-GR123 <juangarros2005@gmail.com>
2025-06-03 21:19:36 +0200

    logo

--- proyecto_final_laravel/resources/views/layouts/app.blade.php
+++ proyecto_final_laravel/resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@ use App\Helpers\Utils;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="{{ asset('favicon.ico') }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     @viteReactRefresh
